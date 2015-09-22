1eb22f691e868a2406e7422cf14f15d044b45f90
Pierre Zurek <pierre.zurek@parrot.com>
2015-09-22 10:57:00 +0200

    [DEV] Add Unix mk files

---
+++ Unix/base/config/product.mk
@@ -0,0 +1,3 @@
+#This file in automatically included by alchemy.
+
+include $(TARGET_CONFIG_DIR)/../../../common/config/Unix/product.mk
---
+++ common/config/Unix/product.mk
@@ -0,0 +1,13 @@
+###############################################################################
+## @file product.mk
+##
+##
+## Product common alchemy variables.
+###############################################################################
+
+# Product common config dir
+COMMON_CONFIG_UNIX_DIR := $(call my-dir)
+
+include $(COMMON_CONFIG_UNIX_DIR)/../common.mk
+
+prebuilt.json.override := 1
