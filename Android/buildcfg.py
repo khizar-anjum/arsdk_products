6c00433f2242787e46029c5e80736fc072d7afb1
Pierre Zurek <pierre.zurek@parrot.com>
2015-10-07 17:30:01 +0200

    [DEV] New build-sdk, build-jni and build-app tasks for Android
    Change-Id: I3dcffd0e5c77117b7dc5b2fbac37628526e6dd37

--- Android/buildcfg.py
+++ Android/buildcfg.py
@@ -4,15 +4,15 @@
 
 import dragon
 import os
+import pprint
 
-def hook_post_buildjni(task, args):
+def build_jni():
     arfreeflight_android_jni_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARFreeFlight", "Android", "ARFreeFlight", "jni")
 
     # Call ndk-build
-    variant = dragon.VARIANT.replace("_", "-")
-    dragon.exec_dir_cmd(dirpath=arfreeflight_android_jni_dir, cmd="ndk-build TARGET_ARCH_ABI=" + variant)
+    dragon.exec_dir_cmd(dirpath=arfreeflight_android_jni_dir, cmd="ndk-build")
 
-def hook_post_buildapp(task, args):
+def build_app():
     arbuildutils_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARBuildUtils")
     arfreeflight_android_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARFreeFlight", "Android")
     arfreeflight_android_script_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARFreeFlight", "Android", "script")
@@ -23,20 +23,49 @@ def hook_post_buildapp(task, args):
     dragon.exec_dir_cmd(dirpath=arfreeflight_android_script_dir, cmd="./generateCompatiblesDevices.py")
     dragon.exec_dir_cmd(dirpath=arfreeflight_android_dir, cmd="./gradlew assembleDebug")
 
+def pre_hook_variant_forall_task(tasklist, tasksargs):
+    dragon.LOGI("pre_hook_variant_forall_task")
+
+def post_hook_variant_forall_task(tasklist, tasksargs):
+    dragon.LOGI("post_hook_variant_forall_task")
+    # dragon.LOGI(pprint.pformat(tasklist))
+    # dragon.LOGI(pprint.pformat(tasksargs))
+    gen_jni = False
+    gen_app = False
+    args_jni = None
+    args_app = None
+
+    if "build-jni" in tasklist:
+        gen_jni = True
+        args_jni = tasksargs[tasklist.index("build-jni")]
+    if "build-app" in tasklist:
+        gen_jni = True
+        gen_app = True
+        args_app = tasksargs[tasklist.index("build-app")]
+
+    if gen_jni:
+        build_jni()
+    if gen_app:
+        build_app()
+
 #===============================================================================
 # Tasks
 #===============================================================================
 
 dragon.add_meta_task(
-    name = "buildjni",
-    desc = "Build SDK & JNI",
+    name = "build-sdk",
+    desc = "Build SDK",
     subtasks = ["build"],
-    posthook = hook_post_buildjni
 )
 
 dragon.add_meta_task(
-    name = "buildapp",
+    name = "build-jni",
+    desc = "Build SDK & JNI",
+    subtasks = ["build-sdk"],
+)
+
+dragon.add_meta_task(
+    name = "build-app",
     desc = "Build SDK, JNI & generate application",
-    subtasks = ["buildjni"],
-    posthook = hook_post_buildapp
+    subtasks = ["build-jni"],
 )
