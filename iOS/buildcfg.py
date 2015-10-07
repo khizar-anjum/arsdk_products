4129ba6a7391cc0f5490b7f55167166a8f452487
Pierre Zurek <pierre.zurek@parrot.com>
2015-10-07 17:51:55 +0200

    [DEV] New build-sdk and build-app tasks for iOS
    Change-Id: I87ffb641d09a0a75cb9c94994d0fc3fd2da6b97a

--- iOS/buildcfg.py
+++ iOS/buildcfg.py
@@ -5,19 +5,41 @@
 import dragon
 import os
 
-def hook_post_buildapp(task, args):
+def build_app():
     arfreeflight_ios_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARFreeFlight", "iOS", "ARFreeFlight")
 
     # Build application
     dragon.exec_dir_cmd(dirpath=arfreeflight_ios_dir, cmd="xcodebuild -project ARFreeFlight.xcodeproj")
+
+def pre_hook_variant_forall_task(tasklist, tasksargs):
+    dragon.LOGI("pre_hook_variant_forall_task")
+
+def post_hook_variant_forall_task(tasklist, tasksargs):
+    dragon.LOGI("post_hook_variant_forall_task")
+    # dragon.LOGI(pprint.pformat(tasklist))
+    # dragon.LOGI(pprint.pformat(tasksargs))
+    gen_app = False
+    args_app = None
+
+    if "build-app" in tasklist:
+        gen_app = True
+        args_app = tasksargs[tasklist.index("build-app")]
+
+    if gen_app:
+        build_app()
 
 #===============================================================================
 # Tasks
 #===============================================================================
 
 dragon.add_meta_task(
-    name = "buildapp",
-    desc = "Build SDK & generate application",
+    name = "build-sdk",
+    desc = "Build SDK",
     subtasks = ["build"],
-    posthook = hook_post_buildapp
+)
+
+dragon.add_meta_task(
+    name = "build-app",
+    desc = "Build SDK & generate application",
+    subtasks = ["build-sdk"],
 )
