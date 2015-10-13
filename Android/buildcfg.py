88f03983cf402b0780d434221448b28509d12278
Nicolas BRULEZ <nicolas.brulez@parrot.com>
2015-10-13 13:26:48 +0200

    [DEV] Added build-release-app task for android
    Change-Id: I9ea7299cef18f0171d956a5c2f5b7493b75f27dc

--- Android/buildcfg.py
+++ Android/buildcfg.py
@@ -12,7 +12,7 @@ def build_jni():
     # Call ndk-build
     dragon.exec_dir_cmd(dirpath=arfreeflight_android_jni_dir, cmd="ndk-build")
 
-def build_app():
+def build_app(release=False):
     arbuildutils_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARBuildUtils")
     arfreeflight_android_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARFreeFlight", "Android")
     arfreeflight_android_script_dir = os.path.join(dragon.WORKSPACE_DIR, "packages", "ARFreeFlight", "Android", "script")
@@ -21,7 +21,10 @@ def build_app():
     dragon.exec_dir_cmd(dirpath=arbuildutils_dir, cmd="./generateAndroidResources.sh")
     dragon.exec_dir_cmd(dirpath=arbuildutils_dir, cmd="./generateLocalPropertiesFile.py")
     dragon.exec_dir_cmd(dirpath=arfreeflight_android_script_dir, cmd="./generateCompatiblesDevices.py")
-    dragon.exec_dir_cmd(dirpath=arfreeflight_android_dir, cmd="./gradlew assembleDebug")
+    if release:
+        dragon.exec_dir_cmd(dirpath=arfreeflight_android_dir, cmd="./gradlew assembleRelease")
+    else:
+        dragon.exec_dir_cmd(dirpath=arfreeflight_android_dir, cmd="./gradlew assembleDebug")
 
 def pre_hook_variant_forall_task(tasklist, tasksargs):
     dragon.LOGI("pre_hook_variant_forall_task")
@@ -32,6 +35,7 @@ def post_hook_variant_forall_task(tasklist, tasksargs):
     # dragon.LOGI(pprint.pformat(tasksargs))
     gen_jni = False
     gen_app = False
+    app_release = False
     args_jni = None
     args_app = None
 
@@ -42,11 +46,16 @@ def post_hook_variant_forall_task(tasklist, tasksargs):
         gen_jni = True
         gen_app = True
         args_app = tasksargs[tasklist.index("build-app")]
+    if "build-release-app" in tasklist:
+        gen_jni = True
+        gen_app = True
+        app_release = True
+        args_app = tasksargs[tasklist.index("build-release-app")]
 
     if gen_jni:
         build_jni()
     if gen_app:
-        build_app()
+        build_app(release=app_release)
 
 #===============================================================================
 # Tasks
@@ -67,5 +76,11 @@ dragon.add_meta_task(
 dragon.add_meta_task(
     name = "build-app",
     desc = "Build SDK, JNI & generate application",
+    subtasks = ["build-jni"],
+)
+
+dragon.add_meta_task(
+    name = "build-release-app",
+    desc = "Build SDK, JNI & generate application in release mode",
     subtasks = ["build-jni"],
 )
