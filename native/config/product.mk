d10700087b5f2c23eed6a1f642c5fa09dc8aec23
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2016-02-16 13:37:07 +0100

    [BUILD] Use our own json version in native build.
    Change-Id: I2593f5f40c72e567e33797751f69515b4d2cf8ed
    Reviewed-on: https://gerrit.parrot.biz/30212
    Reviewed-by: Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>

--- native/config/product.mk
+++ native/config/product.mk
@@ -3,3 +3,6 @@ NATIVE_CONFIG_DIR := $(call my-dir)
 
 # Include common product.mk
 include $(NATIVE_CONFIG_DIR)/../../common/config/product.mk
+
+# Use our own json version
+prebuilt.json.override := 1
