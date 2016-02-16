70e976b1e2d068ac7b4bbced60e8ccf93dec3a1b
Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>
2016-02-16 17:12:28 +0100

    [BUILD] Use our own ncurses version in native build.
    Change-Id: I34ef739b952d4047cce16f2eb6df99a798da9ef6

--- native/config/product.mk
+++ native/config/product.mk
@@ -4,5 +4,6 @@ NATIVE_CONFIG_DIR := $(call my-dir)
 # Include common product.mk
 include $(NATIVE_CONFIG_DIR)/../../common/config/product.mk
 
-# Use our own json version
+# Use our own json, ncurses version
 prebuilt.json.override := 1
+prebuilt.ncurses.override := 1
